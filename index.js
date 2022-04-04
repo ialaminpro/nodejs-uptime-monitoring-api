28d6fad0601a52a17324c313269b0e6398c8de21
Al Amin <ialamin.pro@gmail.com>
2022-04-05 01:49:04 +0600

    Parsing Request

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 // dependencies
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object -  module scaffolding
 const app = {};
@@ -31,9 +32,25 @@ app.handleReqRes = (req, res) => {
     // request handling
     // get the url and parse it
     const parseUrl = url.parse(req.url, true);
-    console.log(parseUrl);
-    // response handle
-    res.end('Hello World');
+    const path = parseUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parseUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // response handle
+        res.end('Hello World');
+    });
 };
 
 // start the server

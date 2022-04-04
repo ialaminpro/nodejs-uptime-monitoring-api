91803c472f1bfc0015e9f3266f5cf983552f716c
Al Amin <ialamin.pro@gmail.com>
2022-04-05 03:33:19 +0600

    Using Environment Variables

--- handlers/routeHandlers/sampleHandler.js
+++ handlers/routeHandlers/sampleHandler.js
@@ -11,7 +11,7 @@
 const handler = {};
 
 handler.sampleHandler = (requestProperties, callback) => {
-    console.log(requestProperties);
+    // console.log(requestProperties);
 
     callback(200, {
         message: 'This is a sample url',
--- index.js
+++ index.js
@@ -10,20 +10,16 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
-
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`listening to port ${environment.port}`);
     });
 };
 

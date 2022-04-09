62c454df8b64c958f9216ebd6029174a3373fcfd
Al Amin <ialamin.pro@gmail.com>
2022-04-09 22:14:53 +0600

    Token update

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -108,7 +108,44 @@ handler._token.get = (requestProperties, callback) => {
 };
 
 handler._token.put = (requestProperties, callback) => {
-    
+    const id =
+        typeof requestProperties.body.id === 'string' &&
+        requestProperties.body.id.trim().length === 20
+            ? requestProperties.body.id
+            : false;
+    const extend =
+    typeof requestProperties.body.extend === 'boolean' &&
+    requestProperties.body.extend === true
+        ? true
+        : false;
+
+    if(id && extend) {
+        data.read('tokens', id, (err, tokenData) => {
+            let tokenObject = parseJSON(tokenData);
+            if(tokenObject.expires > Date.now()){
+                tokenObject.expires = Date.now() + 60 * 60 * 1000;
+
+                // store the updated token
+                data.update('tokens', id, tokenObject, (error) => {
+                    if(!error){
+                        callback(200);
+                    }else{
+                        callback(500, {
+                            error: 'There was a server side error!',
+                        })
+                    }
+                });
+            }else{
+                callback(400, {
+                    error: 'Token already expired!',
+                })
+            }
+        });
+    }else{
+        callback(400, {
+            error: 'There was a problem in your request',
+        })
+    }
 };
 
 handler._token.delete = (requestProperties, callback) => {

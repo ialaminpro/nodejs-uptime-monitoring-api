21697ef4b367c90581c8fb9d9494ca92c52e83e1
Al Amin <ialamin.pro@gmail.com>
2022-04-10 01:13:56 +0600

    Check update

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -35,9 +35,9 @@ handler._check.post = (requestProperties, callback) => {
     let successCodes = typeof requestProperties.body.successCodes === 'object' &&  requestProperties.body.successCodes instanceof Array? requestProperties.body.successCodes : false;
     let timeoutSeconds = typeof requestProperties.body.timeoutSeconds === 'number' && requestProperties.body.timeoutSeconds % 1 === 0 &&  requestProperties.body.timeoutSeconds >= 1 && requestProperties.body.timeoutSeconds <= 5 ? requestProperties.body.timeoutSeconds : false;
     if(protocol && url && method && successCodes && timeoutSeconds) {
-        // verify token
-        let token = typeof requestProperties.headersObject.token === 'string' ? requestProperties.headersObject.token : false;
-        
+    // verify token
+    let token = typeof requestProperties.headersObject.token === 'string' ? requestProperties.headersObject.token : false;
+    
         // lookup the user phone by reading the token
         data.read('tokens', token, (err, tokenData) => {
             if(!err && tokenData){
@@ -154,10 +154,85 @@ handler._check.get = (requestProperties, callback) => {
             error: 'Requested token was not found!',
         });
         
-}
+    }
 };
 
 handler._check.put = (requestProperties, callback) => {
+    const id =
+    typeof requestProperties.body.id === 'string' &&
+    requestProperties.body.id.trim().length === 20
+        ? requestProperties.body.id
+        : false;
+      
+
+    // validate inputs
+    let protocol = typeof requestProperties.body.protocol === 'string' &&  ['http', 'https'].indexOf(requestProperties.body.protocol) > -1 ? requestProperties.body.protocol : false;
+    let url = typeof requestProperties.body.url === 'string' && requestProperties.body.url.trim().length > 0 ? requestProperties.body.url : false;
+    let method = typeof requestProperties.body.method === 'string' &&  ['GET', 'POST', 'PUT', 'DELETE'].indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false;
+    let successCodes = typeof requestProperties.body.successCodes === 'object' &&  requestProperties.body.successCodes instanceof Array? requestProperties.body.successCodes : false;
+    let timeoutSeconds = typeof requestProperties.body.timeoutSeconds === 'number' && requestProperties.body.timeoutSeconds % 1 === 0 &&  requestProperties.body.timeoutSeconds >= 1 && requestProperties.body.timeoutSeconds <= 5 ? requestProperties.body.timeoutSeconds : false;
+    
+    if(id){
+        if(protocol ||  url || method || successCodes || timeoutSeconds) {
+            data.read('checks', id, (err, checkData) => {
+                if(!err && checkData){
+                    let checkObject = parseJSON(checkData);
+                    // verify token
+                     let token = typeof requestProperties.headersObject.token === 'string' ? requestProperties.headersObject.token : false;
+                     tokenHander._token.verify(token, checkObject.userPhone, (tokenIsValid) => {
+                        
+                        if(tokenIsValid){
+                            if(protocol) {
+                                checkObject.protocol = protocol;
+                            }
+                            if(url) {
+                                checkObject.url = url;
+                            }
+                            if(protocol) {
+                                checkObject.method = method;
+                            }
+                            if(successCodes) {
+                                checkObject.successCodes = successCodes;
+                            }
+                            if(timeoutSeconds) {
+                                checkObject.protocol = timeoutSeconds;
+                            }
+                            // store the checkObject
+                            data.update('checks', id, checkObject, (error) => {
+                                if(!error){
+                                    callback(200, {
+                                        message: 'Check was updated successfully!',
+                                    })
+                                }else{
+                                    callback(500, {
+                                        error: 'There was a problem in the server side!',
+                                    });
+                                }
+                            });
+                        }else{
+                            callback(403, {
+                                error: 'Authentication problem!',
+                            });
+                        }
+                    });
+
+                }else{
+                    callback(500, {
+                        error: 'There was a problem in the server side!',
+                    });
+                }
+            })
+        }else{
+            callback(400, {
+                error: 'You must provide at least one field to update!',
+            });
+        }
+    }else{
+        callback(400, {
+            error: 'You have a problem in your request',
+        });
+    }
+       
 };
 
 handler._check.delete = (requestProperties, callback) => {};
